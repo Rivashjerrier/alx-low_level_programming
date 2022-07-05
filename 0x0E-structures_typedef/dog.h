2f51df147eba597d05bd33c869fc31a2e16a1bae
Rivash <rivijerrier@gmail.com>
2022-07-04 17:53:36 -0700

    defines a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - strcut for dog attributes
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner of dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
